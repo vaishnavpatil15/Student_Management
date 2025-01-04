70a46200f31cef7ad22f8d7940225864eb183dfa
Rohit Patil <patilrohit298@gmail.com>
2025-01-04 15:28:40 +0530

    - Updated server file add server URL - Add debug config - Updated user controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,34 +1,25 @@
-const db= require("../models/db")
+const db = require("../models/db")
 
-
-exports.createUser = async (req,res)=>{
+exports.createUser = (req, res) => {
     try {
-        let {
-            f_name,
-            l_name,
-            user_id,
-            dept_id,
-            password
-        }= req.body
+        let { f_name, l_name, user_id, dept_id, password } = req.body
         let createdUserID;
-        let query=`insert into login (user_id,password) values (?,?) `
-        await db.query(query,[user_id,password],(err,result)=>{
-            if(err) throw err;
-            createdUserID=result.insertId //get the id of record inserted
+        let query = `insert into login (user_id,password,created_at) values (?,?) `
+        db.query(query, [user_id, password, new Date()], (err, result) => {
+            if (err) throw err;
+            createdUserID = result.insertId //get the id of record inserted
+            let query1 = `select * from department where id = ?`
+            db.query(query1,[dept_id], (err) => {
+                if (err) throw err;
+                let insertQuery = `insert into users (f_name, l_name, user_id,dept_id,created_at) values(?,?,?,?,?)`
+                db.query(insertQuery, [f_name, l_name, createdUserID, dept_id, new Date()], (err, result) => {
+                    if (err) throw err;
+                    return res.status(201).json({ message: "user created successfully" });
+                })
+            })
         })
-        
-        let query1=`select * from department where id=${dept_id}`
-        await db.query(query1,(err)=>{
-            if(err) throw err;
-        })
-        let insertQuery = `insert into users (f_name, l_name, user_id,dept_id,created_at) values(?,?,?,?,?)`
-        await db.query(insertQuery,[f_name,l_name,createdUserID,dept_id,now()],(err,result)=>{
-            if(err) throw err;
-        })
-        res.status(201).json({message:"user created successfully"})
-
     } catch (err) {
         console.log(err);
-        return res.status(500).json({error:err.message})
+        return res.status(500).json({ error: err.message })
     }
 }
--- server.js
+++ server.js
@@ -10,5 +10,5 @@ app.use('/api/users', userRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port http://localhost:${PORT}`);
 });
