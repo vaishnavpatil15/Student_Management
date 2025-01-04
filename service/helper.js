d496c860ce9ffaf0423313a2789c58a665e6f985
Rohit Patil <patilrohit298@gmail.com>
2025-01-04 21:33:54 +0530

    test code

--- service/helper.js
+++ service/helper.js
@@ -2,6 +2,8 @@ const db = require('../models/db');
 
 exports.getObjectUsingId = (tableName, id) => {
     console.log(`Request to get data for table ${tableName} using id: ${id}`);
+    console.log("branch changes");
+    
     const { id } = req.params;
     const query = `SELECT * FROM ${tableName} WHERE id = ${id}`;
     db.query(query, (err, result) => {
